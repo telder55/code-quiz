0bf2588bab2d1ff557540a66d50bf4b7ecc8b6a4
TJ Elder <telder55@gmail.com>
2021-01-18 09:52:04 -0700

    Created website skeleton with html and css

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// Need a button that starts a timer and also changes the screen. Changing screen can be done by changing the CSS property to alter the visibility to hidden. 
+
+
+// Each question can be four buttons that you can click that have if else statements to show incorrect or correct based on if the correct button is clicked. If correct button is clicked, I'll need to have that counted. 
+
