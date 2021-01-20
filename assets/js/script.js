a641f00d20a99e88b8bfd86af5f71a0a6d150b5e
TJ Elder <telder55@gmail.com>
2021-01-20 11:38:08 -0700

    Created question 1 and 2 and coded functionality to show and hide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,40 @@
 // Selects element by class
 // var timeEl = document.querySelector(".time");
 var timeEl = document.getElementById("timer");
-var startButton = document.getElementById("start-button")
-
-// Selects element by id
-var mainEl = document.getElementById("main");
-
+var startButton = document.getElementById("start-button");
+var correctAnswer = document.querySelector(".correct");
+var incorrectAnswer = document.querySelector(".incorrect");
 var secondsLeft = 60;
 
+
+// Remove Quiz Start
+function removeStart() {
+  var quizStart = document.getElementById("quiz-start");
+  quizStart.classList.remove("active");
+}
+
+// Remove Question 1 Correct
+function removeQ1correct() {
+  q1.classList.remove("active");
+  addQ2 ();
+}
+
+// Add Question 1
+function addQ1() {
+  var q1 = document.getElementById("q1");
+  q1.classList.add("active");
+}
+
+// Add Question 2
+function addQ2() {
+  var q2 = document.getElementById("q2");
+  q2.classList.add("active");
+}
+
 function setTime() {
+  // Calling removeStart function
+    removeStart ();
+    addQ1 ();
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -24,16 +50,12 @@ function setTime() {
   }, 1000);
 }
 
-// Function to create and append colorsplosion image
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
-
-}
-
-// setTime();
 
 // Add event listener to generate button
+// startButton.addEventListener("click", removeStart);
 startButton.addEventListener("click", setTime);
+correctAnswer.addEventListener("click", removeQ1correct );
+incorrectAnswer.addEventListener("click", removeQ1wrong );
+
+
+
