094ffaad1decc70ec3a0eb612389a3f4a9950d58
TJ Elder <telder55@gmail.com>
2021-01-22 17:48:26 -0700

    Correct answer brings up next questions now

--- Homework 4 Notes.js
+++ Homework 4 Notes.js
@@ -1,1 +1,11 @@
-Homework 4 Notes.js
+// Homework 4 Notes.js
+
+
+// To Do
+
+// Need a button that starts a timer and also changes the screen. Changing screen can be done by changing the CSS property to alter the visibility to hidden. 
+
+
+// Each question can be four buttons that you can click that have if else statements to show incorrect or correct based on if the correct button is clicked. If correct button is clicked, I'll need to have that counted. 
+
+//document.queryselector(activeclass) - create a variable 
